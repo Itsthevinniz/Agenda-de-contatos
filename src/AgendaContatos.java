548394c70e742dddce20c7eebc655b8c5a541173
Itsthevinniz <vinicius.ndutra@ufpe.br>
2023-05-08 18:06:37 -0300

    first commit

---
+++ src/AgendaContatos.java
@@ -0,0 +1,24 @@
+public class Pessoa {
+    String nome;
+    String sobrenome;
+    int numero;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
